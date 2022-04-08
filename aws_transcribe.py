21bcee1fa22997f63ae6b9f173585e789c03de18
Ege <79145337+EgedotErcan@users.noreply.github.com>
2022-04-08 13:02:23 +0300

    Transcribe operation was fixed
    Co-Authored-By: Tesla44 <61677105+tesla44@users.noreply.github.com>

--- aws_transcribe.py
+++ aws_transcribe.py
@@ -1,19 +1,13 @@
-import docx2txt 
-from pydoc import text
-from tkinter import W
 import boto3
 import time
 import urllib
 import json
 
-dosya = ""
 
-
-AWS_ACCESS_KEY_ID = 'AKIAUQUQBNZUVYYCH452'
-AWS_SECRET_ACCESS_KEY = '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'
-
-job_name = 'Ertugrulllll'
-    
+current_client = "ertugrul"
+current_waw_file_name = "q1"
+job_name = current_client
+   
 transcribe_client = boto3.client('transcribe', aws_access_key_id="AKIAUQUQBNZUVYYCH452", aws_secret_access_key="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", region_name='us-east-1')
 
 def transcribe_file(job_name, file_uri, transcribe_client):
@@ -23,7 +17,6 @@ def transcribe_file(job_name, file_uri, transcribe_client):
         MediaFormat='wav',
         LanguageCode='en-US'
     )
-
     max_tries = 60
     while max_tries > 0:
         max_tries -= 1
@@ -35,25 +28,21 @@ def transcribe_file(job_name, file_uri, transcribe_client):
                 response = urllib.request.urlopen(job['TranscriptionJob']['Transcript']['TranscriptFileUri'])
                 data = json.loads(response.read())
                 text = data['results']['transcripts'][0]['transcript']
-                print("========== below is output of speech-to-text ========================")
                 print(text) 
-                print("=====================================================================")
                 
             break
         else:
             print(f"Waiting for {job_name}. Current status is {job_status}.")
-        time.sleep(10)
+        time.sleep(4)
 
 
-def main():
-    file_uri = 's3://myexpertunity2/q2.wav'
+def main(current_waw_file_name):
+
+    file_uri = f's3://myexpertunity/{current_waw_file_name}.wav'
     transcribe_file(job_name, file_uri, transcribe_client)
 
 
 if __name__ == '__main__':
-    main()
+    main(current_waw_file_name)
 
 
-dosya2 = open("sesyazi.txt" ,W)
-dosya2.write(dosya)
-    
